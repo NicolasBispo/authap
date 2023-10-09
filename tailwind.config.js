1303e555e6a4dbc2cb4d5a2caae56eff9ba872e6
NicolasBispo <nbm.personalwork@gmail.com>
2023-10-08 23:52:41 -0300

    dependencies and tailwind configuration changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ export default {
       backgroundImage : {
         'lock-bg' : "url('./src/assets/images/lock-bg.jpg')",
         'index-bg' : "url('./src/assets/images/index-bg.svg')"
+      },
+      boxShadow: {
+        'inner-eq': 'inset 0px 0px 30px 0px rgba(0,0,0,0.75)',
       }
     },
   },
